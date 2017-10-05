f1d84d0e6e8bb7b2cf457507233007c9dae3ab73
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2017-10-05 15:27:03 -0300

    Atualizar Calculo.java
    mudança na função da media, e adicionar a função da mediana.

--- Calculo.java
+++ Calculo.java
@@ -5,12 +5,46 @@ public class Calculos {
     private Escaner num;
     private Ampliark amp;
     private Tabela tab;
-    private float media;
+    private float media = 0f;
+    private float mediana = 0f;
    
-    public void media(){
-        for(int c=0; c<amp.getKa(); c++){
-            media = media + (tab.getPontoMedio(c) * tab.getFreqA(c));
+    public Calculos(Escaner n, Ampliark m, Tabela o){
+        this.num = n;
+        this.amp = m;
+        this.tab = o;
+    }
+    
+    public float calcMedia(){
+        for(int c=0; c<tab.getCont(); c++){
+            this.media = media + (tab.getPontoMedio(c)*tab.getFreqA(c));
         }
-        media = media/num.getQtdNum();
+        this.media = media/num.getQtdNum();
+        System.out.println("Media: " + media);
+        return this.media;
+    }
+    
+    public float calcMediana(){
+        int soma = 0;
+        int somaF = 0;
+        amp.calcLargura();
+        for(int c=0; c<tab.getCont(); c++){
+            soma = soma + tab.getFreqA(c);
+            if((num.getQtdNum()/2) < soma){
+                for(int l=0; l<c; l++){
+                    somaF = somaF + tab.getFreqA(l);
+                }
+                mediana = tab.getMin(c) + (((num.getQtdNum()/2)*amp.getLargura())/tab.getFreqA(c));
+            }
+        }
+        System.out.println("Mediana: " + mediana);
+        return this.mediana;
+    }
+    
+    public float getMedia(){
+        return this.media;
+    }
+    
+    public float getMediana(){
+        return this.mediana;
     }
 }
